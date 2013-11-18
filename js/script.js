7ca2c42aa8a897cc4f976ae2f80101a7f68b1c41
carapotts <carapotts@gmail.com>
2013-11-18 09:16:26 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -4,13 +4,39 @@ var drop_array = new Array();
 //vars to name the timers that make drops spawn and move every so often
 var spawntimer;
 var movertimer;
-
+//creating a Bucket obj, instance of the bucket class below. Not on stage yet. 
+var user_bucket = new Bucket(25,200);
 function init(){
+	//put that bucket on the stage
+	user_bucket.create();
+	// when a key is pressed, do a function that responds depending on which key
+	document.onkeydown = function(e){checkKey(e);};
 	//set an interval to do the spawn function every 2 seconds
 	spawntimer = setInterval(spawn,2000);	
 	//set an interval to move the drops 20 times a sec
 	movetimer = setInterval(moveAllDrops,1000/20);	
 }
+
+function checkKey(e){
+	//set up the event again for certain browsers
+	e = e || window.event;
+	//if it's the right arrow key...
+	if(e.keyCode == '39'){
+		//add to bucket's x will move it a little to the right
+		user_bucket.x +=15;
+		//change bucket's style so it moves:
+	user_bucket.item_on_page.style.left = user_bucket.x + "px";
+
+	}
+	if(e.keyCode == '37'){
+		//add to bucket's x will move it a little to the right
+		user_bucket.x -=15;
+		//change bucket's style so it moves:
+	user_bucket.item_on_page.style.left = user_bucket.x + "px";
+	}
+}//close checkKey
+
+
 function spawn() {
 	//make an object based on the Drop Class:
 	var anotherdrop = new Drop();
@@ -27,7 +53,7 @@ function moveAllDrops(){
 		//apply it to the css of the drop on the page
 		currentdrop.item_on_page.style.top = currentdrop.y + "px";
 		//if the drop is low enough, destroy it
-		if(currentdrops.y > 250){
+		if(currentdrop.y > 250){
 			currentdrop.destroy();
 			}
 	}//close for loop
@@ -36,6 +62,8 @@ function moveAllDrops(){
 function Drop(){
 	this.x; //starts empty, will keep track of each Drop's left-right position
 	this.y; //starts empty, will keep track of each Drop's up-down position
+	this.width = 50;
+	this.height = 50;
 	this.item_on_page;
 	/** function does lots of things when a Drop gets created on the page
 	*
@@ -60,7 +88,7 @@ function Drop(){
 		var newsplash = document.createElement("img");
 		newsplash.src = "img/splash.gif?" + Math.random();
 		//set its style to absoule
-		newsplash.style.postion = "absolute";
+		newsplash.style.position = "absolute";
 		newsplash.style.left = this.x + "px";
 		newsplash.style.top = this.y + "px";
 		//attach splash onto the page:
@@ -73,3 +101,32 @@ function Drop(){
 	}
 }
 //close the Class
+
+
+//let's make a Class (blueprint) for each bucket we generate
+function Bucket(x,y){
+	this.x = x; //starts empty, will keep track of each Bucket's left-right position
+	this.y = y; //starts empty, will keep track of each Bucket's up-down position
+	this.width = 100;
+	this.height = 70;
+	this.item_on_page;
+	/** function does lots of things when a Drop gets created on the page
+	*
+	*/
+	this.create = function(){
+		//make a section element in the HTML, store it into the item-on-page we set up above.
+		this.item_on_page = document.createElement("div");
+		
+		//use those x and y coordinates in the CSS to position the Bucket:
+		this.item_on_page.style.left = this.x + "px";
+		this.item_on_page.style.top = this.y + "px";
+		//attach the item to our HTML hierarchy, as a child of the body:
+		document.getElementsByTagName("body")[0].appendChild(this.item_on_page);
+	}
+	/** function does lots of things when a Drop is removed from the page
+	*
+	*/
+	this.destroy = function(){
+	}
+}
+//close the Bucket Class
